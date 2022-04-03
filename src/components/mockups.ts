f36bcb87ca7ab0b603d9adb5dbff04299db41455
lion <zeebha@gmail.com>
2022-04-03 17:59:22 +0200

    add mockups

---
+++ src/components/mockups.ts
@@ -0,0 +1,37 @@
+export const mockups = [
+    {
+        id: '05001',
+        title: 'starfish',
+        date: 238890420,
+        rate: 50,
+        category: ['junior']
+    },
+    {
+        id: '08509',
+        title: 'starship',
+        date: 978343200,
+        rate: 85,
+        category: ['senior', 'junior']
+    },
+    {
+        id: '78055',
+        title: 'starlight',
+        date: 362840400,
+        rate: 78,
+        category: ['senior', 'veteran']
+    },
+    {
+        id: '956331',
+        title: 'stardust',
+        date: 1536682950,
+        rate: 56,
+        category: ['junior']
+    },
+    {
+        id: '77048',
+        title: 'starving',
+        date: 238890420,
+        rate: 70,
+        category: ['veteran']
+    },
+];
