f9aa63466e200b174439271a781efa7200818ba6
lion <zeebha@gmail.com>
2022-05-07 20:08:12 +0200

    format + flex cards

--- src/components/Mozaique.tsx
+++ src/components/Mozaique.tsx
@@ -1,40 +1,35 @@
 import './styles/mozaique.css';
 
 import { spaceships } from './mockups';
-import {ISpaceships} from '../interfaces/interfaceSpaceships';
-
-
+import { ISpaceships } from '../interfaces/interfaceSpaceships';
 
 const Mozaique = () => {
-
-return (
-  <div className='mozaiqueContainer'>
-    {spaceships.map(spaceship => (
-      <Card spaceship={spaceship}/>
-    ))}
-  </div>
+  return (
+    <div className='mozaiqueContainer'>
+      <div className='cardsContainer'>
+        {spaceships.map((spaceship) => (
+          <Card key={spaceship.id} spaceship={spaceship} />
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default Mozaique;
 
-
-
-
-const Card = (props: {spaceship: ISpaceships}) => {
-  const {spaceship} = props;
+const Card = (props: { spaceship: ISpaceships }) => {
+  const { spaceship } = props;
 
   return (
-      <div className='flipCard'>
-          <div className='flipCardInner'>
+    <div className='flipCard'>
+      <div className='flipCardInner'>
+        <div className='flipCardFront'>
+          <div>{spaceship.name}</div>
+          {spaceship.model}
+        </div>
 
-              <div className='flipCardFront'>
-              </div>
-            
-              <div className='flipCardBack'>
-              </div>
-          
-          </div>
+        <div className='flipCardBack'></div>
       </div>
+    </div>
   );
 };
