2899c6c3b5fff836b7c10cbd058baffda9091c5c
lion <zeebha@gmail.com>
2022-05-09 15:29:40 +0200

    fix double quotes

--- src/components/mockups.ts
+++ src/components/mockups.ts
@@ -1,62 +1,62 @@
-import { ISpaceships } from "../interfaces/interfaceSpaceships";
+import { ISpaceships } from '../interfaces/interfaceSpaceships';
 
 export const spaceships: ISpaceships[] = [
-    {
-      id: 52789,
-      model: 'classic',
-      name: "Queen Revenge",
-      lightspeed: true,
-      fuel: "nuclear",
-      crew: {
-        captain: "RuPaul",
-        mecha: "Detox",
-        stowaway: "Latrice"
-      },
+  {
+    id: 52789,
+    model: 'classic',
+    name: 'Queen Revenge',
+    lightspeed: true,
+    fuel: 'nuclear',
+    crew: {
+      captain: 'RuPaul',
+      mecha: 'Detox',
+      stowaway: 'Latrice',
     },
-    {
-      id: 12456,
-      model: 'super',
-      name: "La Dama Negra",
-      lightspeed: true,
-      fuel: "corn",
-      crew: {
-        captain: "Maroon",
-        mecha: "Five"
-      },
+  },
+  {
+    id: 12456,
+    model: 'super',
+    name: 'La Dama Negra',
+    lightspeed: true,
+    fuel: 'corn',
+    crew: {
+      captain: 'Maroon',
+      mecha: 'Five',
     },
-    {
-      id: 23654,
-      model: 'integral',
-      name: "Freelancer",
-      lightspeed: true,
-      fuel: "nuclear",
-      crew: {
-        captain: "Freech",
-        mecha: "Freeda"
-      }
+  },
+  {
+    id: 23654,
+    model: 'integral',
+    name: 'Freelancer',
+    lightspeed: true,
+    fuel: 'nuclear',
+    crew: {
+      captain: 'Freech',
+      mecha: 'Freeda',
     },
-    {
-      id: 78963,
-      model: 'special',
-      name: "El Impoluto",
-      lightspeed: false,
-      fuel: "nuclear",
-      crew: {
-        captain: "Obvious",
-        mecha: "Normal",
-        stowaway: "Out"
-      }
+  },
+  {
+    id: 78963,
+    model: 'special',
+    name: 'El Impoluto',
+    lightspeed: false,
+    fuel: 'nuclear',
+    crew: {
+      captain: 'Obvious',
+      mecha: 'Normal',
+      stowaway: 'Out',
     },
-    {
-        id: 41258,
-        model: 'special',
-        name: "Rewards",
-        lightspeed: true,
-        fuel: "water",
-        crew: {
-          captain: "Green",
-          mecha: "Bean",
-          stowaway: "Carpaccio"
-        }
-      }
-  ];
+  },
+  {
+    id: 41258,
+    model: 'special',
+    name: 'Rewards',
+    lightspeed: true,
+    fuel: 'water',
+    crew: {
+      captain: 'Green',
+      mecha: 'Bean',
+      stowaway: 'Carpaccio',
+    },
+  },
+];
