43c37b00a6f75b7ad07f3144d4e8a23c7e1b5810
lion <zeebha@gmail.com>
2022-05-09 15:42:54 +0200

    add timer to get card to flip number

--- src/components/Mozaique.tsx
+++ src/components/Mozaique.tsx
@@ -1,14 +1,40 @@
 import './styles/mozaique.css';
 
-import { spaceships } from './mockups';
+import { useEffect, useState } from 'react';
+
 import { ISpaceships } from '../interfaces/interfaceSpaceships';
+import { spaceships } from './mockups';
 
 const Mozaique = () => {
+  const [cardToFlip, setCardToFlip] = useState<number | undefined>(undefined);
+
+  function getRandomInteger(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function randomCardToFlip(list: any[]) {
+    const numberOfCards = list.length;
+    const newCardToFlip = getRandomInteger(0, numberOfCards);
+    setCardToFlip(newCardToFlip);
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      randomCardToFlip(spaceships);
+    }, 3500);
+  }, []);
+
   return (
     <div className='mozaiqueContainer'>
       <div className='cardsContainer'>
-        {spaceships.map((spaceship) => (
-          <Card key={spaceship.id} spaceship={spaceship} />
+        {spaceships.map((spaceship, index) => (
+          <Card
+            key={spaceship.id}
+            spaceship={spaceship}
+            isFlipped={index === cardToFlip}
+          />
         ))}
       </div>
     </div>
@@ -17,14 +43,17 @@ const Mozaique = () => {
 
 export default Mozaique;
 
-const Card = (props: { spaceship: ISpaceships }) => {
-  const { spaceship } = props;
+const Card = (props: { spaceship: ISpaceships; isFlipped: boolean }) => {
+  const { spaceship, isFlipped } = props;
 
   return (
     <div className='flipCard'>
       <div className='flipCardInner'>
-        <FrontCard spaceship={spaceship} />
-        <BackCard spaceship={spaceship} />
+        {isFlipped ? (
+          <BackCard spaceship={spaceship} />
+        ) : (
+          <FrontCard spaceship={spaceship} />
+        )}
       </div>
     </div>
   );
