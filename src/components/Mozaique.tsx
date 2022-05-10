caaebc013ff65a29b2df09896c978005958edfa7
elodie <elodie.koob@affilae.com>
2022-05-10 15:32:27 +0200

    add pause between each card flip

--- src/components/Mozaique.tsx
+++ src/components/Mozaique.tsx
@@ -6,7 +6,11 @@ import {ISpaceships} from '../interfaces/interfaceSpaceships';
 import {spaceships} from './mockups';
 
 const Mozaique = () => {
+  const numberOfCards = spaceships.length;
+  const newCardToFlip = getRandomInteger(0, numberOfCards);
+
   const [cardToFlip, setCardToFlip] = useState<number | undefined>(undefined);
+  const [isBreakOn, setIsBreakOn] = useState<boolean>(false);
 
   function getRandomInteger(min: number, max: number) {
     min = Math.ceil(min);
@@ -14,17 +18,24 @@ const Mozaique = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function randomCardToFlip(list: any[]) {
-    const numberOfCards = list.length;
-    const newCardToFlip = getRandomInteger(0, numberOfCards);
+  function randomCardToFlip() {
     setCardToFlip(newCardToFlip);
+    setIsBreakOn(!isBreakOn);
+  }
+
+  function putOnBreak() {
+    setIsBreakOn(!isBreakOn);
+    setCardToFlip(undefined);
   }
 
   useEffect(() => {
-    setInterval(() => {
-      randomCardToFlip(spaceships);
-    }, 3500);
-  }, []);
+    if (!isBreakOn) setTimeout(randomCardToFlip, 1500);
+  }, [isBreakOn]);
+
+
+  useEffect(() => {
+    if (isBreakOn) setTimeout(putOnBreak, 3500);
+  }, [isBreakOn]);
 
 
   return (
@@ -85,7 +96,7 @@ const Card = (props: {spaceship: ISpaceships; isFlipped: boolean}) => {
     back: {
       width: '100%',
       height: '100%',
-      backgroundColor: '#02b6fe',
+      backgroundColor: 'orange',
       backfaceVisibility: 'hidden',
       WebkitBackfaceVisibility: 'hidden',
       left: '0',
