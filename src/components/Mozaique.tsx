e0c4adc4137c75fac2680dfee8160a1b3ca02898
lion <zeebha@gmail.com>
2022-05-13 14:00:04 +0200

    apply style as inline-css + animation

--- src/components/Mozaique.tsx
+++ src/components/Mozaique.tsx
@@ -1,6 +1,6 @@
 import './styles/mozaique.css';
 
-import { useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 
 import { ISpaceships } from '../interfaces/interfaceSpaceships';
 import { spaceships } from './mockups';
@@ -46,15 +46,72 @@ export default Mozaique;
 const Card = (props: { spaceship: ISpaceships; isFlipped: boolean }) => {
   const { spaceship, isFlipped } = props;
 
+  const frontRotateY = `rotateY(${isFlipped ? 180 : 0}deg)`;
+  const backRotateY = `rotateY(${isFlipped ? 0 : -180}deg)`;
+
+  const styles: Record<string, CSSProperties> = {
+    container: {
+      width: 220,
+      height: 220,
+      backgroundColor: 'transparent',
+      border: '1px solid white',
+      borderRadius: 10,
+      margin: 10,
+      perspective: 1000,
+    },
+    inner: {
+      height: '100%',
+      width: '100%',
+      position: 'relative',
+    },
+    front: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 10,
+      backgroundColor: '#009ddd',
+      boxShadow: 'rgb(0 0 0 / 13%) 0px 4px 31px',
+      backfaceVisibility: 'hidden',
+      WebkitBackfaceVisibility: 'hidden',
+      left: '0',
+      top: '0',
+      position: isFlipped ? 'absolute' : 'relative',
+      transformStyle: 'preserve-3d',
+      transform: frontRotateY,
+      transition: '0.8s',
+    },
+    back: {
+      width: 'calc(100% - 40px)',
+      height: 'calc(100% - 40px)',
+      padding: 20,
+      borderRadius: 10,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      backgroundColor: '#02b6fe',
+      boxShadow: 'rgb(0 0 0 / 13%) 0px 4px 31px',
+      backfaceVisibility: 'hidden',
+      WebkitBackfaceVisibility: 'hidden',
+      left: '0',
+      top: '0',
+      position: isFlipped ? 'relative' : 'absolute',
+      //position: 'absolute',
+      transformStyle: 'preserve-3d',
+      transform: backRotateY,
+      transition: '0.8s',
+    },
+  };
+
   return (
-    <div className='flipCard'>
-      <div className='flipCardInner'>
-        <div id='frontCard'>
+    <div id='container' style={styles.container}>
+      <div id='inner' style={styles.inner}>
+        <div id='frontCard' style={styles.front}>
           <div>{spaceship.name}</div>
           <div>{spaceship.model}</div>
         </div>
 
-        <div id='backCard'>crew</div>
+        <div id='backCard' style={styles.back}>
+          crew
+        </div>
       </div>
     </div>
   );
